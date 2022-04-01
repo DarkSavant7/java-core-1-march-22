7e695342fb71647d971e8f78c1e1ab042dbb7787
Alex Grigorev <persian89g@gmail.com>
2022-04-01 22:00:12 +0300

    lesson 5

---
+++ src/main/java/ru/gb/java_core1/l3_practice/Example.java
@@ -0,0 +1,13 @@
+package ru.gb.java_core1.l3_practice;
+
+import ru.gb.java_core1.l5_oop.Cat;
+
+public class Example {
+    public static void main(String[] args) {
+//        Cat cat3 = new Cat("Vaska", "grey", 1);
+//        System.out.println(cat3);
+//        cat3.run();
+//        cat3.run(3000);
+//        cat3.voice();
+    }
+}
---
+++ src/main/java/ru/gb/java_core1/l5_oop/Classwork.java
@@ -0,0 +1,56 @@
+package ru.gb.java_core1.l5_oop;
+
+public class Classwork {
+    public static void main(String[] args) {
+//        Cat cat = new Cat();
+//        cat.name = "Murzik";
+//        cat.age = 2;
+//        cat.color = "red";
+//
+//        Cat cat2 = new Cat();
+//        cat2.name = "Barsik";
+//        cat2.age = 3;
+//        cat2.color = "black";
+//        System.out.println(cat);
+//
+//        cat.age = 999;
+//        cat.color = "white";
+//        System.out.println(cat);
+//        System.out.println(cat2);
+//
+//        cat.run();
+//        cat.run(1000);
+//        cat.voice();
+//
+//        cat2.run();
+//        cat2.run(1000);
+//        cat2.voice();
+
+        Cat cat3 = new Cat("Vaska", "grey", 1);
+        System.out.println(cat3);
+        cat3.run();
+        cat3.run(3000);
+        cat3.voice();
+
+        cat3.setName("Murka");
+        System.out.println(cat3);
+
+        Cat.doStatic();
+        System.out.println(Cat.type);
+
+        cat3.nonStatic();
+
+        cat3.doStatic();
+        System.out.println(cat3.type);
+        Cat cat4 = new Cat("Tom", "grey", 1);
+
+        System.out.println(cat4.type);
+
+        cat4.type = "KLJBKHBKJB";
+
+        System.out.println(cat4.type);
+        System.out.println(cat3.type);
+
+    }
+
+}
